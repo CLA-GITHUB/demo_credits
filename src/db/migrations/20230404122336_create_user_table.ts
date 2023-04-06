281bfddde99c8e0392c4651d1df4bd76ecb4a665
CLA-GITHUB <christwealthla@gmail.com>
2023-04-06 14:11:45 +0100

    Update knex config for production

--- src/db/migrations/20230404122336_create_user_table.ts
+++ src/db/migrations/20230404122336_create_user_table.ts
@@ -3,7 +3,7 @@ export async function up(knex: Knex): Promise<void> {
   let uuidGenerationRaw =
     knex.client.config.client === "sqlite3"
       ? `(lower(hex(randomblob(4))) || '-' || lower(hex(randomblob(2))) || '-4' || substr(lower(hex(randomblob(2))),2) || '-' || substr('89ab',abs(random()) % 4 + 1, 1) || substr(lower(hex(randomblob(2))),2) || '-' || lower(hex(randomblob(6))))`
-      : `uuid_generate_v4()`;
+      : `(UUID())`;
   return knex.schema.createTable("users", (table) => {
     table.uuid("id").primary().defaultTo(knex.raw(uuidGenerationRaw));
     table.string("name").notNullable();
--- src/db/migrations/20230404123949_create_account_table.ts
+++ src/db/migrations/20230404123949_create_account_table.ts
@@ -4,7 +4,7 @@ export async function up(knex: Knex): Promise<void> {
   let uuidGenerationRaw =
     knex.client.config.client === "sqlite3"
       ? `(lower(hex(randomblob(4))) || '-' || lower(hex(randomblob(2))) || '-4' || substr(lower(hex(randomblob(2))),2) || '-' || substr('89ab',abs(random()) % 4 + 1, 1) || substr(lower(hex(randomblob(2))),2) || '-' || lower(hex(randomblob(6))))`
-      : `uuid_generate_v4()`;
+      : `(UUID())`;
   return knex.schema.createTable("accounts", (table) => {
     table.uuid("id").primary().defaultTo(knex.raw(uuidGenerationRaw));
     table.string("account_number").notNullable().unique();
