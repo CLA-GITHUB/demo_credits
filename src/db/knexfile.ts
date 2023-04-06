2ff725269da495bee763bb166695e7d7bbabb864
CLA-GITHUB <christwealthla@gmail.com>
2023-04-06 23:40:14 +0100

    Add test config for jest

--- src/db/knexfile.ts
+++ src/db/knexfile.ts
@@ -1,7 +1,7 @@
 import type { Knex } from "knex";
 import dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config({ path: "../../.env" });
 // Update with your config settings.
 
 const extension =
@@ -13,10 +13,25 @@ const config: { [key: string]: Knex.Config } = {
   development: {
     client: "sqlite3",
     connection: {
-      filename: process.env.DEV_DB_PATH!,
+      filename:
+        process.env.DEV_DB_PATH ||
+        "C:\\Users\\USER\\Desktop\\dev stuff\\Node\\be_tasks\\demo_credit\\src\\db\\demo_db.db3",
     },
     migrations: {
-      directory: ".src/db/migrations",
+      directory: "./src/db/migrations",
+    },
+    useNullAsDefault: true,
+  },
+
+  test: {
+    client: "sqlite3",
+    connection: {
+      filename:
+        process.env.DEV_DB_PATH_TEST! ||
+        "C:\\Users\\USER\\Desktop\\dev stuff\\Node\\be_tasks\\demo_credit\\src\\db\\test_db.db3",
+    },
+    migrations: {
+      directory: "./src/db/migrations",
     },
     useNullAsDefault: true,
   },
@@ -24,15 +39,15 @@ const config: { [key: string]: Knex.Config } = {
   production: {
     client: "mysql2",
     connection: {
-      host: process.env.HOST || "containers-us-west-118.railway.app",
-      user: process.env.USER || "root",
-      password: process.env.PASSWORD || "yihB7QpuNzRf7rU2vZh6",
-      database: process.env.DATABASE || "railway",
-      port: (process.env.DB_PORT as unknown as number) || 5476,
+      host: process.env.HOST,
+      user: process.env.USER,
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE,
+      port: process.env.DB_PORT as unknown as number,
     },
     migrations: {
       tableName: "knex_migrations",
-      directory: "./migrations",
+      directory: "./src/db/migrations",
       extension: extension,
       loadExtensions: [`.${extension}`],
     },
