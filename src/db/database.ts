2722da17bf9545ef0d3e78c9164788371f766008
CLA-GITHUB <christwealthla@gmail.com>
2023-04-06 09:21:19 +0100

    Add knex config files

---
+++ src/db/database.ts
@@ -0,0 +1,22 @@
+import knex, { Knex } from "knex";
+import config from "./knexfile";
+
+export default class Database {
+  protected connection: Knex;
+
+  constructor() {
+    this.connection = knex(config[process.env.NODE_ENV!]);
+  }
+
+  async close() {
+    await this.connection.destroy();
+  }
+
+  async dropAllTables() {
+    this.connection.schema.dropTableIfExists("users");
+    this.connection.schema.dropTableIfExists("accounts");
+    this.connection.schema.dropTableIfExists("transactions");
+
+    console.log("all tables dropped");
+  }
+}
