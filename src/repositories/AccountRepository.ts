8c845eb33e4456157bb2db6b9a39e5e45ac5e811
CLA-GITHUB <christwealthla@gmail.com>
2023-04-06 09:32:53 +0100

    Add class that communicates with account  table in db

---
+++ src/repositories/AccountRepository.ts
@@ -0,0 +1,114 @@
+import Database from "../db/database";
+import Account from "../models/account";
+import Transaction from "../models/transaction";
+import { TAccount } from "../types/types";
+import TransactionRepository from "./TransactionRepository";
+
+export default class AccountRepository extends Database {
+  constructor() {
+    super();
+  }
+
+  async getAccount(filter: string): Promise<Account | null> {
+    const account = await this.connection<Account>("accounts")
+      .where("id", filter)
+      .orWhere("user_id", filter)
+      .orWhere("account_number", filter)
+      .first();
+
+    if (!account) {
+      return null;
+    }
+
+    return account;
+  }
+
+  async createAccount(account: Account) {
+    await this.connection<Account>("accounts").insert(account);
+    return;
+  }
+
+  async getCurrentBalance(userId: string): Promise<number> {
+    const balance = await this.connection<Account>("accounts")
+      .where("user_id", userId)
+      .select("balance");
+    return balance[0].balance!;
+  }
+
+  async fundAccount(userId: string, amount: number): Promise<void> {
+    const transactionRepository = new TransactionRepository();
+    // get the current balance
+    const currentBalance = await this.getCurrentBalance(userId);
+
+    await this.connection.transaction(async (trx) => {
+      //increment the balance
+      trx("accounts")
+        .where("user_id", userId)
+        .update({ balance: currentBalance + amount })
+        .then(async () => {
+          const transaction = new Transaction({
+            receiver: userId,
+            amount,
+            type: "deposit",
+          });
+          //creates transaction
+          await transactionRepository.create(transaction);
+        });
+    });
+
+    return;
+  }
+
+  async transferFunds({
+    sender,
+    receiver,
+    amount,
+    type,
+  }: {
+    sender: string;
+    receiver: string;
+    amount: number;
+    type: string;
+  }): Promise<void> {
+    const sendersBalance = await this.getCurrentBalance(sender);
+    const receiversBalance = await this.getCurrentBalance(receiver);
+    const transactionProvider = this.connection.transactionProvider();
+    const transaction = await transactionProvider();
+
+    await transaction<Account>("accounts")
+      .where("user_id", sender)
+      .update({
+        balance: sendersBalance - amount,
+      });
+
+    await transaction<Account>("accounts")
+      .where("user_id", receiver)
+      .update({
+        balance: receiversBalance + amount,
+      });
+
+    await transaction<Transaction>("transactions").insert({
+      receiver,
+      sender,
+      amount,
+      type,
+    });
+
+    await transaction.commit();
+    return;
+  }
+
+  async withdrawFunds(userId: string, amount: number): Promise<void> {
+    const transactionRepository = new TransactionRepository();
+    const userBalance = await this.getCurrentBalance(userId);
+
+    await this.connection<Account>("accounts")
+      .where("user_id", userId)
+      .update("balance", amount);
+    await transactionRepository.create({
+      amount,
+      type: "withdrawal",
+      sender: userId,
+    });
+  }
+}
